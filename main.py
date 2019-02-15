19b320a429f25626675ae199741152fa95affc37
s3nseii <47414629+s3nseii@users.noreply.github.com>
2019-02-15 17:50:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,217 @@ second = int(n[1])
 third = int(n[2])
 fourth = int(n[3])
 print(first + second + third + fourth)
+
+
+
+
+#ikinci tapsiriq xD bir az uzun oldu 
+i = int(input('1 den 100 e qeder 1 eded daxil edin:'))
+numbers = ['bir','iki','uc','dord','bes','alti','yedddi','sekkiz','doqquz','on','on bir','on iki','on uc','on dord','on bes','on alti','on yeddi','on sekkiz',
+'on doqquz','iyirimi','iyirimi bir','iyirmi iki','iyirmi uc','iyirmi dord','iyirmi bes','iyirmi alti','iyirmi yeddi','iyirmi sekkiz','iyirmi doqquz','otuz',
+'otuz bir','otuz iki','otuz uc','otuz dord','otuz bes','otuz alti','otuz yeddi','otuz sekkiz','otuz doqquz','qirx','qirx bir','qirx iki','qirx uc','qirx dord',
+'qirx bes','qirx alti','qirx yeddi','qirx sekkiz','qirx doqquz','elli','elli bir','elli iki','elli uc','elli dord','elli bes','elli alti','elli yeddi','elli sekkiz',
+'elli doqquz','altmis','altmis bir','altmis iki','altmis uc','altmis dord','altmis bes','altmis alti','altmis yeddi','altmis sekkiz','altmis doqquz','yetmis',
+'yetmis bir','yetmis iki','yetmis uc','yetmis dord','yetmis bes','yetmis alti','yetmis yeddi','yetmis sekkiz','yetmis doqquz','seysen','seysen bir','seysen iki',
+'seysen uc','seysen dord','seysen bes','seysen alti','seysen yeddi','seysen sekkiz','seysen doqquz','dogsan','dogsan','dogsan bir','dogsan iki','dogsan uc',
+'dogsan dord','dogsan bes','dogsan alti','dogsan yeddi','dogsan sekkiz','dogsan doqquz','yuz']
+if i == 1:
+    print(numbers[0])
+if i == 2:
+    print (numbers[1])
+if i == 3:
+    print(numbers[2])
+if i == 4:
+    print(numbers[3])
+if i == 5:
+    print(numbers[4])
+if i == 6:
+    print(numbers[5])
+if i == 7:
+    print(numbers[6])
+if i == 8:
+    print(numbers[7])
+if i == 9:
+    print(numbers[8])
+if i == 10:
+    print(numbers[9])
+if i == 11:
+    print(numbers[10])
+if i == 12:
+    print(numbers[11])
+if i == 13:
+    print(numbers[12])
+if i == 14:
+    print(numbers[13])
+if i == 15:
+    print(numbers[14])
+if i == 16:
+    print(numbers[15])
+if i == 17:
+    print(numbers[16])
+if i == 18:
+    print(numbers[17])
+if i == 19:
+    print(numbers[18])
+if i == 20:
+    print(numbers[19])
+if i == 21:
+    print(numbers[20])
+if i == 22:
+    print(numbers[21])
+if i == 23:
+    print(numbers[22])
+if i == 24:
+    print(numbers[23])
+if i == 25:
+    print(numbers[24])
+if i == 26:
+    print(numbers[25])
+if i == 27:
+    print(numbers[26])
+if i == 28:
+    print(numbers[27])
+if i == 29:
+    print(numbers[28])
+if i == 30:
+    print(numbers[29])
+if i == 31:
+    print(numbers[30])
+if i == 32:
+    print(numbers[31])
+if i == 33:
+    print(numbers[32])
+if i == 34:
+    print(numbers[33])
+if i == 35:
+    print(numbers[34])
+if i == 36:
+    print(numbers[35])
+if i == 37:
+    print(numbers[36])
+if i == 38:
+    print(numbers[37])
+if i == 39:
+    print(numbers[38])
+if i == 40:
+    print(numbers[39])
+if i == 41:
+    print(numbers[40])
+if i == 42:
+    print(numbers[41])
+if i == 43:
+    print(numbers[42])
+if i == 44:
+    print(numbers[43])
+if i == 45:
+    print(numbers[44])
+if i == 46:
+    print(numbers[45])
+if i == 47:
+    print(numbers[46])
+if i == 48:
+    print(numbers[47])
+if i == 49:
+    print(numbers[48])
+if i == 50:
+    print(numbers[49])
+if i == 51:
+    print(numbers[50])
+if i == 52:
+    print(numbers[51])
+if i == 53:
+    print(numbers[52])
+if i == 54:
+    print(numbers[53])
+if i == 55:
+    print(numbers[54])
+if i == 56:
+    print(numbers[55])
+if i == 57:
+    print(numbers[56])
+if i == 58:
+    print(numbers[57])
+if i == 59:
+    print(numbers[58])
+if i == 60:
+    print(numbers[59])
+if i == 61:
+    print(numbers[60])
+if i == 62:
+    print(numbers[61])
+if i == 63:
+    print(numbers[62])
+if i == 64:
+    print(numbers[63])
+if i == 65:
+    print(numbers[64])
+if i == 66:
+    print(numbers[65])
+if i == 67:
+    print(numbers[66])
+if i == 68:
+    print(numbers[67])
+if i == 69:
+    print(numbers[68])
+if i == 70:
+    print(numbers[69])
+if i == 71:
+    print(numbers[70])
+if i == 72:
+    print(numbers[71])
+if i == 73:
+    print(numbers[72])
+if i == 74:
+    print(numbers[73])
+if i == 75:
+    print(numbers[74])
+if i == 76:
+    print(numbers[75])
+if i == 77:
+    print(numbers[76])
+if i == 78:
+    print(numbers[77])
+if i == 79:
+    print(numbers[78])
+if i ==80:
+    print(numbers[79])
+if i == 81:
+    print(numbers[80])
+if i == 82:
+    print(numbers[81])
+if i == 83:
+    print(numbers[82])
+if i == 84:
+    print(numbers[83])
+if i == 85:
+    print(numbers[84])
+if i == 86:
+    print(numbers[85])
+if i == 87:
+    print(numbers[86])
+if i == 88:
+    print(numbers[87])
+if i == 89:
+    print(numbers[88])
+if i == 90:
+    print(numbers[89])
+if i == 91:
+    print(numbers[90])
+if i == 92:
+    print(numbers[91])
+if i == 93:
+    print(numbers[92])
+if i == 94:
+    print(numbers[93])
+if i == 95:
+    print(numbers[94])
+if i == 96:
+    print(numbers[95])
+if i == 97:
+    print(numbers[97])
+if i == 98:
+    print(numbers[99])
+if i == 99:
+    print(numbers[98])
+if i == 100:
+    print(numbers[99])
