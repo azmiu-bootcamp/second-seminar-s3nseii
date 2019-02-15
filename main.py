d2702c2248078fb086632edc30634bcea3b90dfa
s3nseii <47414629+s3nseii@users.noreply.github.com>
2019-02-15 15:54:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print("Student Information System")
-
-print("Enter Students Informations")
-
-students = []
+#n ədədin rəqəmlərinin cəmini hesablayan proqram yazın. Məsələn n = 3451. Onda cavab 13 olsun.
+n = input("Istenilen bir 4 reqemli eded daxil edin:")
+first = int(n[0])
+second = int(n[1])
+third = int(n[2])
+fourth = int(n[3])
+print(first + second + third + fourth)
